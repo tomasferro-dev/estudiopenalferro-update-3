7aed462ca833a33af15e220b9534e845b5f766fb
tomasferro-dev <tomasferro.dev>
2026-01-31 00:49:22 -0300

    loader

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './app/App'
 import "./styles/index.css";
 
+const loader = document.getElementById("loader");
+if (loader) {
+  loader.style.opacity = "0";
+  loader.style.pointerEvents = "none";
+  setTimeout(() => loader.remove(), 300);
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
